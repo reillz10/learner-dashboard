fde075dbed9506d776bc92ac2dc329c527066766
Douglas Hall <dhall@edx.org>
2018-09-26 15:52:48 -0400

    Get rid of unused reducers.

--- src/data/reducers/index.js
+++ src/data/reducers/index.js
@@ -1,11 +1,6 @@
 import { combineReducers } from 'redux';
 
-import posts from './posts';
-import comment from './comment';
-
 const rootReducer = combineReducers({
-  posts,
-  comment,
 });
 
 export default rootReducer;
